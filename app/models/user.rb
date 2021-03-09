6e81279b2f4c8c42f4624b56a183c5f9846d56d4
ayman <aymanjaber2012@gmail.com>
2021-03-09 15:43:17 +0200

    Validate User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :email, presence: true
+
+
+    validates :username,  :presence => true, 
+                    :length => {:minimum => 1, :maximum => 50}
+                   
+    validates :email, :presence => true, 
+                    :length => {:minimum => 3, :maximum => 50},
+                    :uniqueness => true,
+                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
