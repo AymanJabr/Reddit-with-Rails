281f8db39adab3ce6f9b5e48f2d0d2088299a2cb
FrederickMih <fredtimermih@gmail.com>
2021-03-11 13:00:22 +0100

    Modified comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :post, through: :user 
- 
+  belongs_to :post
+  belongs_to :user
+
   validates :body,  :presence => true, 
                     :length => {:minimum => 1, :maximum => 200}
 end
